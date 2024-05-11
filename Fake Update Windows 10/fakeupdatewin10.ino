a66eb8d41542606088869f4a3d9322e0ba693df7
Redix45 <151219905+Redix45@users.noreply.github.com>
2024-05-11 14:47:35 +0200

    Update fakeupdatewin10.ino

--- Fake Update Windows 10/fakeupdatewin10.ino
+++ Fake Update Windows 10/fakeupdatewin10.ino
@@ -3,12 +3,6 @@ void setup() {
   //empty
 }
 void loop() {
-  DigiKeyboard.delay(2000);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(600);
-  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ?t=43s");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(5000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(3000);
